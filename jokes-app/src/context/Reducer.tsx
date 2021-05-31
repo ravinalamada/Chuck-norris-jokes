d00c2d228b6ebddd8215697afef81e5e09e99552
prisca <prisca.ros@onja.org>
2021-05-31 15:42:37 +0300

    Added reducers

---
+++ jokes-app/src/context/Reducer.tsx
@@ -0,0 +1,34 @@
+import { Action, State } from '../utils/Types'
+
+const reducer = (state: State, action: Action) => {
+  switch (action.type) {
+    case 'FETCH_RANDOM_JOKES':
+      return {
+        ...state,
+        loading: false,
+        jokes: action.payload,
+      }
+    case 'FETCH_JOKES_WITH_CATEGORY':
+      return {
+        ...state,
+        loading: false,
+        jokes: action.payload,
+      }
+    case 'SET_NAME_TO_REPLACE_NAME':
+      return {
+        ...state,
+        loading: false,
+        nameToReplace: action.payload,
+      }
+    case 'SET_CATEGORY':
+      return {
+        ...state,
+        loading: false,
+        category: action.payload,
+      }
+    default:
+      return state
+  }
+}
+
+export default reducer
